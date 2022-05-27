16ac1e0e86e0487b6b7271172f29d7986407f7a1
Young-TW <young20050727@gmail.com>
2022-05-27 15:41:58 +0800

    調整大小和長寬

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import ModelViewer from './view/modelViewer';
+import './view/modelBox.css';
 
 function App() {
   return (
     <div className="App">
-      <ModelViewer scale="40" modelPath={"/tree.glb"} />
+      <div className='modelbox'>
+        <ModelViewer scale="5" modelPath={"/tree.glb"} />
+      </div>
     </div>
   );
 }
