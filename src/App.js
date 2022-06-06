66c01c3ee076116897bba63d088ce734452def03
Young-TW <young20050727@gmail.com>
2022-06-06 12:54:29 +0800

    add my github 2021 contribution 3d

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <div className='modelbox'>
-        <ModelViewer scale="5" modelPath={"/tree.glb"} />
+        <ModelViewer scale="40" modelPath={"/tree.glb"} />
+        <ModelViewer scale="2" modelPath={"/young2021.glb"} />
       </div>
     </div>
   );
